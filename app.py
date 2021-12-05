634882d5683befa12ecf0dc429f7b9902340d2fb
Metleb1996 <metlebrustemov1996@gmail.com>
2021-12-05 15:44:59 +0400

    Restructured

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/post")
+def post():
+    return render_template("post.html")
+
+@app.route("/lr")
+def lr():
+    return render_template("lr.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
