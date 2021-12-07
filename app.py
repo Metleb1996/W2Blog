86bb4054ad030ab4e396e63ba348814c761470ed
Metleb1996 <metlebrustemov1996@gmail.com>
2021-12-07 19:16:34 +0400

    Edit page added

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
+import datetime
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    pub_date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category = db.relationship('Category', backref=db.backref('posts', lazy=True))
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
 
 @app.route("/")
 def index():
@@ -13,6 +45,10 @@ def post():
 @app.route("/lr")
 def lr():
     return render_template("lr.html")
+
+@app.route("/edit")
+def edit():
+    return render_template("edit.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
