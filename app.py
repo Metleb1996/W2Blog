033454be067351a948f9c7911c7d5e3ffccef3ed
Metleb1996 <metlebrustemov1996@gmail.com>
2021-12-11 22:43:43 +0400

    Added Registration and Login forms

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import datetime
-from flask import Flask, render_template, redirect, url_for, abort
+import string
+import re
+import random
+from flask import Flask, render_template, redirect, url_for, session, request, abort
 from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, BooleanField, StringField, PasswordField, EmailField, SubmitField, validators
+from flask_wtf.file import FileField, FileRequired
 
 
 app = Flask(__name__)
@@ -43,6 +48,7 @@ class Category(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.Integer, default=0) # 0, 1, 2, 3, 4, 5  (0-quest, 5-super_admin)
     username = db.Column(db.String(30), unique=True, nullable=False)
     fullname = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -63,12 +69,36 @@ class Comment(db.Model):
     article_id = db.Column(db.Integer, db.ForeignKey('article.id'))
 
 
+class RegistrationForm(Form):
+    username = StringField('User Name', [validators.DataRequired(), validators.Length(min=4, max=25)], render_kw={"class":"form-control","maxlength":"25","minlength":"4"})
+    email = EmailField('Email address', [validators.DataRequired(), validators.Length(min=8, max=35), validators.Email(message=(u'That\'s not a valid email address.'))], render_kw={"class":"form-control","maxlength":"35","minlength":"8"})
+    password = PasswordField('Password', [validators.DataRequired(), validators.Length(min=10, max=35), validators.EqualTo('confirm', message='Passwords must match')], render_kw={"class":"form-control","maxlength":"35","minlength":"10"})
+    confirm = PasswordField('Repeat Password', [validators.DataRequired()], render_kw={"class":"form-control"})
+    submitbutton = SubmitField("Register")
+
+class LoginForm(Form):
+    email = EmailField('Email address', [validators.DataRequired(), validators.Length(min=8, max=35), validators.Email(message=(u'That\'s not a valid email address.'))], render_kw={"class":"form-control","maxlength":"35","minlength":"8"})
+    password = PasswordField('Password', [validators.DataRequired(), validators.Length(min=10, max=35)], render_kw={"class":"form-control","maxlength":"35","minlength":"10"})
+    submitbutton = SubmitField("Login")
+
+def csrf_text(size=32, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def is_email(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if re.fullmatch(regex, email):
+        return True
+    return False
 
 @app.route("/")
 @app.route("/category/<int:cat_id>")
 def index(cat_id=None):
     w2b_context.clear()
-    w2b_context.update({"user":{"user_id":-1}}) #! Add user controll system
+    if "user_id" in session:
+        user = User.query.filter_by(id=session['user_id'])
+        w2b_context.update({"user":{"user_id":session['user_id'], "user":user}})
+    else:
+        w2b_context.update({"user":{"user_id":-1}}) 
     w2b_context.update({"categories":Category.query.all()})
     if cat_id == None :
         w2b_context.update({"articles":Article.query.all(), "category":-1})
@@ -85,41 +115,75 @@ def index(cat_id=None):
 @app.route("/post/<int:id>")
 def post(id=None):
     w2b_context.clear()
-    w2b_context.update({"user":{"user_id":-1}}) #! Add user controll system
+    if "user_id" in session:
+        user = User.query.filter_by(id=session['user_id'])
+        w2b_context.update({"user":{"user_id":session['user_id'], "user":user}})
+    else:
+        w2b_context.update({"user":{"user_id":-1}}) 
     if id == None:
         return redirect(url_for('index'))
     article = Article.query.filter_by(id=id).first()
     w2b_context.update({"article":article})
     return render_template("post.html", cntxt=w2b_context)
 
-@app.route("/lr")
+@app.route("/lr", methods=['POST', 'GET'])
 def lr():
+    if request.method == "POST":
+        # TODO
+        return redirect(url_for('index'))
     w2b_context.clear()
-    w2b_context.update({"user":{"user_id":-1}}) #! Add user controll system
+    if "user_id" in session:
+        session.pop('user_id', None) 
+    rform = RegistrationForm()
+    lform = LoginForm()
+    csrf_token = csrf_text()
+    session["csrf_token"] = csrf_token
+    w2b_context.update({"user":{"user_id":-1}, "csrf_token":csrf_token, "lform":lform, "rform":rform})
     return render_template("lr.html", cntxt=w2b_context)
+
+@app.route("/logout")
+def logout():
+	session.pop('user_id', None)
+	return redirect(url_for('index'))
 
 @app.route("/edit")
 def edit():
     w2b_context.clear()
-    w2b_context.update({"user":{"user_id":-1}}) #! Add user controll system
+    if "user_id" in session:
+        user = User.query.filter_by(id=session['user_id'])
+        w2b_context.update({"user":{"user_id":session['user_id'], "user":user}})
+    else:
+        w2b_context.update({"user":{"user_id":-1}}) 
     return render_template("edit.html", cntxt=w2b_context)
 
 @app.route("/about")
 def about():
     w2b_context.clear()
-    w2b_context.update({"user":{"user_id":-1}}) #! Add user controll system
+    if "user_id" in session:
+        user = User.query.filter_by(id=session['user_id'])
+        w2b_context.update({"user":{"user_id":session['user_id'], "user":user}})
+    else:
+        w2b_context.update({"user":{"user_id":-1}}) 
     return abort(404)
 
 @app.route("/projects")
 def projects():
     w2b_context.clear()
-    w2b_context.update({"user":{"user_id":-1}}) #! Add user controll system
+    if "user_id" in session:
+        user = User.query.filter_by(id=session['user_id'])
+        w2b_context.update({"user":{"user_id":session['user_id'], "user":user}})
+    else:
+        w2b_context.update({"user":{"user_id":-1}}) 
     return abort(404)
 
 @app.route("/gallery")
 def gallery():
     w2b_context.clear()
-    w2b_context.update({"user":{"user_id":-1}}) #! Add user controll system
+    if "user_id" in session:
+        user = User.query.filter_by(id=session['user_id'])
+        w2b_context.update({"user":{"user_id":session['user_id'], "user":user}})
+    else:
+        w2b_context.update({"user":{"user_id":-1}}) 
     return abort(404)
 
 if __name__ == "__main__":
