019a32a521023063997bdfdaee396f4368b23482
Metleb1996 <metlebrustemov1996@gmail.com>
2022-07-14 11:11:28 +0400

    Fixed UPLOAD_FOLDER bug

--- core/models/singletons.py
+++ core/models/singletons.py
@@ -5,7 +5,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__, template_folder="../../templates", static_folder="../../static")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vt.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['UPLOAD_FOLDER'] = "{}/static/media".format(app.root_path)
+app.config['UPLOAD_FOLDER'] = "{}/../../static/media".format(app.root_path)
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024 #uploaded file max size
 app.secret_key = b'HbfGMYwEOnP3oVEbbnPuoYxx1FHPdLSoNKku3qmKfWUjt6tsLdm3USo5k7JRWmXNiGIjpyXtm7DZ1DbAYAzn0g8LmerBW1DsaeSf'
 db = SQLAlchemy(app)
